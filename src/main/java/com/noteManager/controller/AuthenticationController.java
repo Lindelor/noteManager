745ca7c19dc4ffbe5a4913e67704a92abc794ac0
Lindelor <leontev150@mail.ru>
2024-05-01 19:46:38 +0400

    Token management added

---
+++ src/main/java/com/noteManager/controller/AuthenticationController.java
@@ -0,0 +1,32 @@
+package com.noteManager.controller;
+
+import com.noteManager.dto.AuthRequest;
+import com.noteManager.utils.JWTUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api")
+public class AuthenticationController {
+    @Autowired
+    private JWTUtils jwtUtils;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping(path = "/login")
+    public String create(@RequestBody AuthRequest authRequest) {
+        var authentication = new UsernamePasswordAuthenticationToken(
+                authRequest.getUsername(), authRequest.getPassword());
+
+        authenticationManager.authenticate(authentication);
+
+        var token = jwtUtils.generateToken(authRequest.getUsername());
+        return token;
+    }
+}
